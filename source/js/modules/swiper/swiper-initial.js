25f10888a944e021b35dc5b5e7b4e356b74b07ec
Roman Tsarew <romats64@gmail.com>
2023-07-28 19:38:21 +0200

    done tours block

--- source/js/modules/swiper/swiper-initial.js
+++ source/js/modules/swiper/swiper-initial.js
@@ -1,4 +1,5 @@
 const trainersSlider = document.querySelector('.intro__swiper');
+const toursSlider = document.querySelector('.tours__slider');
 
 function swiperIntroTurnOn() {
 
@@ -18,4 +19,43 @@ function swiperIntroTurnOn() {
   }
 }
 
-export {swiperIntroTurnOn};
+function swiperToursTurnOn() {
+
+  if (toursSlider) {
+    // eslint-disable-next-line no-undef, no-unused-vars
+    const swiper = new Swiper(toursSlider, {
+      initialSlide: 0,
+      cssMode: true,
+      direction: 'horizontal',
+      loop: false,
+
+      breakpoints: {
+        // when window width is >= 1200px
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 18,
+          initialSlide: 0,
+        },
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+          initialSlide: 0,
+        },
+      },
+
+      navigation: {
+        nextEl: '.tours__button--next',
+        prevEl: '.tours__button--prev',
+      },
+
+    });
+  }
+}
+
+export {swiperIntroTurnOn, swiperToursTurnOn};
